7f2fa8f6dddab2b7301231ed4de1d4015a894e92
KajiyamaVK <victor.kajiyama@gmail.com>
2026-01-02 15:52:16 -0300

    Forces x11 for keybindings

--- linux/runner/main.cc
+++ linux/runner/main.cc
@@ -1,6 +1,8 @@
 #include "my_application.h"
+#include <X11/Xlib.h> // <--- Add this import
 
 int main(int argc, char** argv) {
+  XInitThreads(); // <--- Add this call FIRST
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
